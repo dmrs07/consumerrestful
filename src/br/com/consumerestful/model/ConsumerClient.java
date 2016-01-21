150b3f8ebfd8c2fb0b1818d3f7e4a006c12bee87
Daniel Moraes <dmrs07@gmail.com>
2016-01-21 00:57:30 -0200

    tesete
    Signed-off-by: Daniel Moraes <dmrs07@gmail.com>

---
+++ src/br/com/consumerestful/model/ConsumerClient.java
@@ -0,0 +1,32 @@
+package br.com.consumerestful.model;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+
+
+public class ConsumerClient {
+
+	public static void main(String[] args) {
+		Client client = ClientBuilder.newClient();
+		
+		
+		WebTarget target = client.target("http://localhost:8080/Restful/cliente/listarTodos");
+		
+		String stringJson = target.request(MediaType.APPLICATION_JSON).get(String.class);
+		System.out.println(stringJson);
+		
+		
+		
+			Gson gson = new Gson();
+			final Cliente cliente = gson.fromJson(stringJson, Cliente.class);
+			
+			
+			System.out.println(cliente.getNome());		
+
+	}
+
+}
