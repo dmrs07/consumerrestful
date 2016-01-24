4ac49f24378b69cc5d9e26663d1f3b45e9f75c4b
Daniel Moraes <dmrs07@gmail.com>
2016-01-24 19:37:34 -0200

    Correção

---
+++ src/br/com/consumerestful/model/JSON.java
@@ -0,0 +1,46 @@
+package br.com.consumerestful.model;
+
+public class JSON {
+	Cliente cliente;
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+		return result;
+	}
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JSON other = (JSON) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "JSON [cliente=" + cliente + "]";
+	}
+	
+	
+}
